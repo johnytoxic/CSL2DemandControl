f62248bbbb06b8414c40242e6962927a9975ce96
johnytoxic <johnytoxic@protonmail.com>
2024-04-07 11:59:34 +0200

    re-integrate code into new mod prject (template)

---
+++ InfiniteDemand/InfiniteDemandMod.cs
@@ -0,0 +1,24 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+
+namespace InfiniteDemand
+{
+    public class InfiniteDemandMod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(InfiniteDemand)}.{nameof(InfiniteDemandMod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            var harmony = new HarmonyLib.Harmony("net.johnytoxic.infinitedemand");
+            harmony.PatchAll();
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+    }
+}
